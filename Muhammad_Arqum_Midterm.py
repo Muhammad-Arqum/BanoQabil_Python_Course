4770845d3ec1c212ac20ef05a82dcbd36e077592
Muhammad-Arqum <155262915+Muhammad-Arqum@users.noreply.github.com>
2024-01-21 09:36:07 +0500

    Muhammad_Arqum_Midterm.py

--- Muhammad_Arqum_Midterm.py
+++ Muhammad_Arqum_Midterm.py
@@ -12,16 +12,22 @@ power = int(num1**num2)
 print('power ',power)
 floor_divident = int(num1%num2)
 print('floor_division',floor_divident)
+
 # Q2)Write a Python program to find the area of a triangle.
+
 length = float(input('enter the length of a triangle'))
 height = float(input('enter the height of a triangle'))
 area = float(length*height/2)
 print('the area of triangle is',area)
+
 # Q3)Write a Python program to convert Celsius to Fahrenheit.
+
 celsius = float(37)
 fehranhite  = float((celsius*(9/5))+32)
 print('37 degree fehranhite is equal to 98.6 degree farenheite',fehranhite)
+
 # Q4)Write a Python program that return all datatypes that we discussed in the class.
+
 print('the data type of 19 is:',type(19))
 print('the data type of 16.5 is:',type(16.5))
 print('the data type of muhammad arqum is:',type('muhammad arqum'))
